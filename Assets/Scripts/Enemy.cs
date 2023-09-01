44fa27eb83121bfbdf29d86eaecfd073a991c34c
Parva <parvadesai404@gmail.com>
2023-09-01 21:44:06 +0530

    fix: Fixed the face flipping logic
    Simplified the face flipping logic with just a boolean instead of a direction based calculation.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,10 +8,9 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private Transform[] patrolSpots;
     private int currPatrolSpotIndex = 0;
-    [SerializeField] private float stopDistance = 0.1f;
-    
-    private bool canMove = true;
-    private bool isMovingRight = false;
+    [SerializeField] private float minStopDistance = 0.2f;
+
+    private bool isFacingRight = false;
 
     private void Awake()
     {
@@ -21,7 +20,6 @@ public class Enemy : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (canMove)
             Move();
     }
     
@@ -37,57 +35,51 @@ public class Enemy : MonoBehaviour
     {
         Vector2 currPos = transform.position;
         Vector2 movePos = new Vector2(patrolSpots[currPatrolSpotIndex].position.x, patrolSpots[currPatrolSpotIndex].position.y);
-
+        
         // If we are closer to our target than our minimum distance...
-        if (Vector2.Distance(currPos, movePos) <= stopDistance)
+        if (Vector2.Distance(currPos, movePos) <= minStopDistance)
         {
             // Update to the next target
             currPatrolSpotIndex += 1;
 
-            // If we've gone past the end of our list...
-            // (if our current point index is equal or bigger than
-            // the length of our list)
+            // If we've gone past the end of our array... (if our current point index is equal or bigger than the length of our list)
             if (currPatrolSpotIndex >= patrolSpots.Length)
             {
-                // ...loop back to the start by setting 
-                // the current point index to 0
+                // ...loop back to the start by setting the current index to 0
                 currPatrolSpotIndex = 0;
             }
         }
 
-        // Now, move in the direction of our target
-
-        // Get the direction
-        // Subtract the current position from the target position to get a distance vector
-        // Normalise changes it to be length 1, so we can then multiply it by our speed / force
-        Vector2 direction = ((Vector2)patrolSpots[currPatrolSpotIndex].position - (Vector2)transform.position).normalized;
-
-        if (direction.x > 0f)
-        {
-            Flip();
-        }
-        else if (direction.x < 0f)
-        {
-            Flip();
-        }
+        //Get the normalised direction vector bw target & curr pos
+        Vector2 direction = (movePos - currPos).normalized;
         
-        // Move in the correct direction with the set force strength
-        //rb.AddForce(direction * forceStrength);
+        bool movingRight = IsMovingRight(direction);
+        
+        // If new patrol direction is none that means obj has hit the patrol spot
+        if (movingRight != isFacingRight)
+        {
+            Flip();
+        }
+        isFacingRight = IsMovingRight(direction);
+        
+        // Move towards the target position with given speed
         currPos = Vector2.MoveTowards(currPos, movePos, speed * Time.deltaTime);
         transform.position = currPos;
     }
-
+    
+    // Flips the game object to face it in the same direction its moving
     private void Flip()
     {
-        if (isMovingRight)
+        transform.eulerAngles = isFacingRight ? new Vector3(0, -180, 0) : new Vector3(0, 0, 0);
+    }
+    
+    private bool IsMovingRight(Vector2 direction)
+    {
+        return direction switch
         {
-            transform.eulerAngles = new Vector3(0, -180, 0);
-            isMovingRight = false;
-        }
-        else
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-            isMovingRight = true;
-        }
+            { x: > 0} => true,
+            { x: < 0} => false,
+            _ => false,
+        };
     }
 }
